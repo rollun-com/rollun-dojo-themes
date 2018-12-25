ee0d49dd4fe324248304c435ab9b44aa2cd507c3
bryakwork <bryakwork@gmail.com>
2018-12-25 20:44:23 +0200

    initial commit

---
+++ src/default/index.ts
@@ -0,0 +1,49 @@
+import * as accordionPane from './widgets/accordion-pane.m.css';
+import * as button from './widgets/button.m.css';
+import * as calendar from './widgets/calendar.m.css';
+import * as checkbox from './widgets/checkbox.m.css';
+import * as combobox from './widgets/combobox.m.css';
+import * as dialog from './widgets/dialog.m.css';
+import * as icon from './widgets/icon.m.css';
+import * as label from './widgets/label.m.css';
+import * as listbox from './widgets/listbox.m.css';
+import * as progress from './widgets/progress.m.css';
+import * as radio from './widgets/radio.m.css';
+import * as select from './widgets/select.m.css';
+import * as slidePane from './widgets/slide-pane.m.css';
+import * as slider from './widgets/slider.m.css';
+import * as splitPane from './widgets/split-pane.m.css';
+import * as tabController from './widgets/tab-controller.m.css';
+import * as textArea from './widgets/text-area.m.css';
+import * as textInput from './widgets/text-input.m.css';
+import * as enhancedTextInput from './widgets/enhanced-text-input.m.css';
+import * as timePicker from './widgets/time-picker.m.css';
+import * as titlePane from './widgets/title-pane.m.css';
+import * as toolbar from './widgets/toolbar.m.css';
+import * as tooltip from './widgets/tooltip.m.css';
+
+export default {
+	'@dojo/widgets/accordion-pane': accordionPane,
+	'@dojo/widgets/button': button,
+	'@dojo/widgets/calendar': calendar,
+	'@dojo/widgets/checkbox': checkbox,
+	'@dojo/widgets/combobox': combobox,
+	'@dojo/widgets/dialog': dialog,
+	'@dojo/widgets/icon': icon,
+	'@dojo/widgets/label': label,
+	'@dojo/widgets/listbox': listbox,
+	'@dojo/widgets/progress': progress,
+	'@dojo/widgets/radio': radio,
+	'@dojo/widgets/select': select,
+	'@dojo/widgets/slide-pane': slidePane,
+	'@dojo/widgets/slider': slider,
+	'@dojo/widgets/split-pane': splitPane,
+	'@dojo/widgets/tab-controller': tabController,
+	'@dojo/widgets/text-area': textArea,
+	'@dojo/widgets/text-input': textInput,
+	'@dojo/widgets/enhanced-text-input': enhancedTextInput,
+	'@dojo/widgets/time-picker': timePicker,
+	'@dojo/widgets/title-pane': titlePane,
+	'@dojo/widgets/toolbar': toolbar,
+	'@dojo/widgets/tooltip': tooltip
+};
